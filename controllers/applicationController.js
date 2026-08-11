38e39ca7252c07070636ee11155ddf701c33895e
Sajan Gupta <kumarsajan986@gmail.com>
2026-08-11 19:21:48 +0530

    created getMyApplications, getJobApplications

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Application = require("../models/Application.js");
 const Job = require("../models/Job.js");
 
@@ -43,4 +44,68 @@ const applyJob = async (req, res) => {
     }
 };
 
-module.export = { applyJob };
+const getMyApplications = async (req, res) => {
+
+    try {
+        const userId = req.user.id;
+
+        const applications = await Application.find({
+            user: userId
+        })
+            .populate("job", "title company location salary role")
+            .sort({ appliedAt: -1 })
+
+        return res.status(200).json({
+            applications
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+const getJobApplications = async (req, res) => {
+
+    try {
+        const jobId = req.params.jobId;
+
+        if (!mongoose.Types.ObjectId.isValid(jobId)) {
+            return res.status(400).json({
+                message: "Invalid Job ID"
+            })
+        }
+
+        const job = await Job.findById(jobId);
+        if (!job) {
+            return res.status(404).json({
+                message: "Job not found"
+            });
+        }
+
+        if (job.createdBy.toString() !== req.user.id.toString()) {
+            return res.status(403).json({
+                message: "Your are not allowed to view applicant"
+            })
+        }
+
+        const applicants = await Application.find({
+            job: jobId
+        })
+            .populate("user", "name email")
+            .sort({ appliedAt: -1 });
+
+
+        return res.status(200).json({
+            applicants
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+module.exports = { applyJob, getMyApplications, getJobApplications };
