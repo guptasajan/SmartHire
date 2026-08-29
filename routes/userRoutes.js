1ed6724f7cf87a78c414b405d49080e3b73e629c
Sajan Gupta <kumarsajan986@gmail.com>
2026-08-29 13:33:20 +0530

    Update project

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 
-const upload = require("../middleware/upload.js");
+//const upload = require("../middleware/upload.js");
 
 
 
