2522760386f39dc97fa95d9b3ea934d2cfa7d738
Sajan Gupta <kumarsajan986@gmail.com>
2026-09-04 19:06:29 +0530

    Connect frontend to deployed backend

--- smarthire-frontend/src/services/api.js
+++ smarthire-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://smarthire-backend-gpzt.onrender.com/api",
 });
 
 // Request Interceptor
